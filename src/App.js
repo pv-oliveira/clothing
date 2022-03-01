b7e716d74ca10c59c281b512e23c46adfdec8cc7
Paulo Victor <paulo.victor.ig@hotmail.com>
2022-03-01 15:01:02 -0300

    implementei firebase e a habilidade de armazenar users autenticados na database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.components';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import Header from './components/header/header.components';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor(){
@@ -20,9 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser:user })
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+      this.setState({currentUser: userAuth});
+      // createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount(){
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
+
 const config = {
     apiKey: "AIzaSyCQ4zQ68-cYl5fyf09JxY55i_BwMzoYPLA",
     authDomain: "shopping-8cbfa.firebaseapp.com",
@@ -11,6 +12,36 @@ const config = {
     appId: "1:324047470381:web:ef9be18c717739ad999da4",
     measurementId: "G-YD6GLPD2K6"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+ 
+   const userRef = firestore.collection('users').doc(userAuth.uid);
+ 
+   const snapShop = await userRef.get();
+ 
+   if (!snapShop.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+ 
+      try {
+         await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData,
+         });
+ 
+         console.log('user creation successfull.');
+      } catch (error) {
+         console.log('Error creating user', error.message);
+      }
+   }
+ 
+   return userRef;
+};
+
+
 
 firebase.initializeApp(config);
 
